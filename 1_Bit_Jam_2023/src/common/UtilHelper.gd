22458201d3aafbd1988f01ee180397456070c8c2
Dan Lungaro <Chunk_77@hotmail.com>
2023-11-16 21:41:20 -0800

    added overworld tutorial

--- 1_Bit_Jam_2023/src/common/UtilHelper.gd
+++ 1_Bit_Jam_2023/src/common/UtilHelper.gd
@@ -5,5 +5,7 @@ static func get_player_portrait(class_type: Player.ClassType) -> Texture:
 	match class_type:
 		Player.ClassType.KNIGHT:
 			return Textures.knight_portrait
+		Player.ClassType.WARLOCK:
+			return Textures.warlock_portrait
 		_:
 			return Textures.knight_portrait
